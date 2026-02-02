c8493d5006863636e8f0c2b0fd6a1869daaee078
Daniel Nguyen <nguyendaniel1312@gmail.com>
2026-02-02 01:25:58 -0800

    add cursorignore for envs

--- timesheets.py
+++ timesheets.py
@@ -3,7 +3,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from dotenv import load_dotenv
+import os
 import time
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Set up Chrome options to avoid bot detection
 options = Options()
@@ -11,21 +16,28 @@ options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
 
+# Get credentials from environment variables
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+
 # Set up the browser
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 try:
-    # Go to a website (example: Google)
+    # Go to the login page
     driver.get("https://www.etimesheets.ihss.ca.gov/login")
     
-    # # Find the search box by its name attribute
-    # search_box = driver.find_element(By.NAME, "q")
+    # Wait for page to load
+    time.sleep(2)
     
-    # # Type something into it
-    # search_box.send_keys("Hello, world!")
-    
-    # # Submit the form
-    # search_box.submit()
+    # TODO: Find and fill in the login form
+    # Example (you'll need to inspect the page to get the correct element IDs):
+    # username_field = driver.find_element(By.ID, "username")
+    # password_field = driver.find_element(By.ID, "password")
+    # username_field.send_keys(username)
+    # password_field.send_keys(password)
+    # login_button = driver.find_element(By.ID, "login-button")
+    # login_button.click()
     
     # Wait for user to finish (press Enter to close)
     input("\nBrowser is open. Press Enter to close...")
